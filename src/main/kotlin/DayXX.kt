8f7c8f2e90bd57d446fe7d792555d6ec5bd26f64
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2022-12-01 08:44:52 +0100

    Add file for a new day

---
+++ src/main/kotlin/DayXX.kt
@@ -0,0 +1,14 @@
+fun solveDayXX_1(input: String): Int {
+    return 0
+}
+
+fun main() {
+    val inputExample = readFile("dayXX_example.txt")
+    val inputTask = readFile("dayXX.txt")
+
+    println("Solution for task 1 example: ${solveDayXX_1(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDayXX_1(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDayXX_1(inputExample)}") // ???
+    println("Solution for task 2 task:    ${solveDayXX_1(inputTask)}") // ???
+}
+
