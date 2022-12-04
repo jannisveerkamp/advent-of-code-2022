eaed50e0536406b85a0a0c4a26e6746cb458f1c6
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2022-12-04 21:33:22 +0100

    Improve day 3

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,37 +1,24 @@
-fun solveDay03a(input: String): Int {
-    val lines = input.split("\n")
-    return lines.sumOf { line ->
-        val first = line.substring(0, line.length / 2)
-        val second = line.substring(line.length / 2)
-        val common = first.toCharArray().toSet().intersect(second.toCharArray().toSet()).first()
-
-        // Lowercase item types a through z have priorities 1 through 26.
-        // Uppercase item types A through Z have priorities 27 through 52.
-        val value = if (common.isUpperCase()) {
-            common - 'A' + 27
-        } else {
-            common - 'a' + 1
-        }
-        value
-    }
+fun solveDay03a(input: String): Int = input.split("\n").sumOf { line ->
+    intersection(
+        line.substring(0, line.length / 2).toSet(),
+        line.substring(line.length / 2).toSet()
+    )
 }
 
-fun solveDay03b(input: String): Int {
-    val lines = input.split("\n").chunked(3)
-    return lines.sumOf { chunk ->
-        val first = chunk[0]
-        val second = chunk[1]
-        val third = chunk[2]
-        val common = first.first { char ->
-            second.contains(char) && third.contains(char)
-        }
-        val value = if (common.isUpperCase()) {
-            common - 'A' + 27
-        } else {
-            common - 'a' + 1
-        }
-        value
-    }
+fun solveDay03b(input: String): Int = input.split("\n").chunked(3).sumOf { chunk ->
+    intersection(chunk[0].toSet(), chunk[1].toSet(), chunk[2].toSet())
+}
+
+private fun intersection(vararg list: Set<Char>): Int = priority(list.reduce { acc, it -> acc.intersect(it) }.first())
+
+/**
+ * Lowercase item types a through z have priorities 1 through 26.
+ * Uppercase item types A through Z have priorities 27 through 52.
+ */
+private fun priority(common: Char): Int = if (common.isUpperCase()) {
+    common - 'A' + 27
+} else {
+    common - 'a' + 1
 }
 
 fun main() {
