7c3796e5c70f734eae54151407d60c2ba0e76592
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2022-12-13 13:23:15 +0100

    Some more code improvements

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -4,51 +4,46 @@ private val json = Json {
     isLenient = true
 }
 
-private sealed interface Element {
-    data class ArrayElement(val children: List<Element>) : Element
-    data class Primitive(val value: Int) : Element
-}
+private sealed interface Element
+private data class ArrayElement(val children: List<Element>) : Element
+private data class Primitive(val value: Int) : Element
 
 private fun parsePackets(packet: JsonElement): Element = when (packet) {
-    is JsonArray -> Element.ArrayElement(packet.map { parsePackets(it) })
-    is JsonPrimitive -> Element.Primitive(packet.content.toInt())
+    is JsonArray -> ArrayElement(packet.map { parsePackets(it) })
+    is JsonPrimitive -> Primitive(packet.content.toInt())
     else -> error("unknown type: $packet")
 }
 
-private fun comparePackets(firstPacket: Element, secondPacket: Element): Int {
+private fun compare(first: Element, second: Element): Int {
     return when {
-        firstPacket is Element.Primitive && secondPacket is Element.Primitive ->
-            secondPacket.value.compareTo(firstPacket.value)
-        firstPacket is Element.ArrayElement && secondPacket is Element.ArrayElement -> {
-            firstPacket.children.zip(secondPacket.children).forEach {
-                val x = comparePackets(it.first, it.second)
-                if (x != 0) {
-                    return x
+        first is Primitive && second is Primitive -> second.value.compareTo(first.value)
+        first is ArrayElement && second is ArrayElement -> {
+            first.children.zip(second.children).forEach {
+                val compare = compare(it.first, it.second)
+                if (compare != 0) {
+                    return compare
                 }
             }
-            if (firstPacket.children.size == secondPacket.children.size) {
-                return 0
-            } else if (firstPacket.children.size > secondPacket.children.size) {
-                return -1
+            if (first.children.size == second.children.size) {
+                0
+            } else if (first.children.size > second.children.size) {
+                -1
             } else {
-                return 1
+                1
             }
         }
-        firstPacket is Element.Primitive && secondPacket is Element.ArrayElement ->
-            comparePackets(Element.ArrayElement(listOf(firstPacket)), secondPacket)
-        firstPacket is Element.ArrayElement && secondPacket is Element.Primitive ->
-            comparePackets(firstPacket, Element.ArrayElement(listOf(secondPacket)))
+        first is Primitive && second is ArrayElement -> compare(ArrayElement(listOf(first)), second)
+        first is ArrayElement && second is Primitive -> compare(first, ArrayElement(listOf(second)))
         else -> error("should never happen")
     }
 }
 
 fun solveDay13a(input: String): Int {
-    val packetPairs = input.split("\n\n")
-    return packetPairs.withIndex().sumOf { (index, packetPair) ->
+    return input.split("\n\n").withIndex().sumOf { (index, packetPair) ->
         val (firstPacketRaw, secondPacketRaw) = packetPair.split("\n")
         val firstPacket = parsePackets(json.parseToJsonElement(firstPacketRaw))
         val secondPacket = parsePackets(json.parseToJsonElement(secondPacketRaw))
-        if (comparePackets(firstPacket, secondPacket) != -1) {
+        if (compare(firstPacket, secondPacket) != -1) {
             index + 1
         } else {
             0
@@ -62,22 +57,20 @@ fun solveDay13b(input: String): Int {
 
     packetPairs.forEach { packetPair ->
         val (firstPacketRaw, secondPacketRaw) = packetPair.split("\n")
-        val firstPacket = parsePackets(json.parseToJsonElement(firstPacketRaw))
-        val secondPacket = parsePackets(json.parseToJsonElement(secondPacketRaw))
-        allPacketPairs.add(firstPacket)
-        allPacketPairs.add(secondPacket)
+        allPacketPairs.add(parsePackets(json.parseToJsonElement(firstPacketRaw)))
+        allPacketPairs.add(parsePackets(json.parseToJsonElement(secondPacketRaw)))
     }
 
-    val first = Element.ArrayElement(listOf(Element.ArrayElement(listOf(Element.Primitive(2)))))
-    allPacketPairs.add(first)
-    val second = Element.ArrayElement(listOf(Element.ArrayElement(listOf(Element.Primitive(6)))))
-    allPacketPairs.add(second)
+    val dividerPacket1 = ArrayElement(listOf(ArrayElement(listOf(Primitive(2)))))
+    allPacketPairs.add(dividerPacket1)
+    val dividerPacket2 = ArrayElement(listOf(ArrayElement(listOf(Primitive(6)))))
+    allPacketPairs.add(dividerPacket2)
 
     allPacketPairs.sortWith(Comparator { o1: Element, o2: Element ->
-        return@Comparator -comparePackets(o1, o2)
+        return@Comparator -compare(o1, o2)
     })
 
-    return (allPacketPairs.indexOf(first) + 1) * (allPacketPairs.indexOf(second) + 1)
+    return (allPacketPairs.indexOf(dividerPacket1) + 1) * (allPacketPairs.indexOf(dividerPacket2) + 1)
 }
 
 fun main() {
