883d2d0ae0a733e8ae81816472d7edeee1e16c4a
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2022-12-15 16:51:48 +0100

    Add solution for 15

--- src/main/kotlin/common/Point.kt
+++ src/main/kotlin/common/Point.kt
@@ -1,3 +1,7 @@
 package common
 
-data class Point(val x: Int, val y: Int)
+import kotlin.math.abs
+
+data class Point(val x: Int, val y: Int) {
+    fun manhattan(other: Point): Int = abs(other.x - x) + abs(other.y - y)
+}
